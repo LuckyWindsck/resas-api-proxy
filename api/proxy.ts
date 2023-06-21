e830febb24b69404704ac5fbb2c8ba320e27312a
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-21 09:03:28 +0900

    feat: allow cors

--- api/proxy.ts
+++ api/proxy.ts
@@ -10,5 +10,13 @@ export default async (request: Request) => {
   const response = await fetch(apiURL, { headers: { 'X-API-KEY': RESAS_API_KEY } })
   const json = (await response.json()) as object
 
-  return new Response(JSON.stringify(json))
+  return new Response(JSON.stringify(json), {
+    headers: {
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers':
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    },
+  })
 }
